1719dbaea08aec04cc01791f2c8908c757cbf606
seab <seab@seab-CV5>
2022-01-28 10:56:27 -0500

    Reorganized

--- ebird-media-sorter/src/main/java/fun/seabird/MediaSorter.java
+++ ebird-media-sorter/src/main/java/fun/seabird/MediaSorter.java
@@ -2,6 +2,7 @@ package fun.seabird;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -47,21 +48,10 @@ public abstract class MediaSorter
 	
 	private static final Set<String> audioExtensions = ImmutableSet.of("wav","mp3","m4a");
 	private static final Set<String> videoExtensions = ImmutableSet.of("mov","m4v","mp4");
-	private static final Set<String> imageExtensions = ImmutableSet.of("jpg","jpeg");
+	private static final Set<String> imageExtensions = ImmutableSet.of("jpg","jpeg");	
 	
-	public static void main(String[] args) throws IOException, ImageProcessingException
+	private static void parseCsv(String csvPath) throws FileNotFoundException, IOException
 	{
-		if (args.length < 2)
-		{
-			System.out.println("USAGE: 1st argument is path to directory that contains MyEBirdData.csv file (e.g. downloads/ebird). 2nd argument is path to media directory.");
-			return;
-		}
-		Long hrsOffset=0l;
-		if (args.length == 3)
-			hrsOffset = Long.valueOf(args[2]);
-		
-		String csvPath = args[0];		
-		
 		//Initialize RangeMap for quickly finding subId for a date
 		try(FileInputStream myDataStream = new FileInputStream(csvPath + File.separator + "MyEBirdData.csv");
 			CsvListReader csvListReader = new CsvListReader(new InputStreamReader(myDataStream,"UTF-8"),CsvPreference.STANDARD_PREFERENCE))
@@ -108,134 +98,148 @@ public abstract class MediaSorter
 			}
 			System.out.println("DONE parsing!\nTraversing directory...");
 		}	
+	}
+	
+	private static void checkMetadataAndMove(File f,String outputPath,Long hrsOffset,Set<String> subIds) throws IOException
+	{
+		if (f.isDirectory() || f.getPath().contains("ebird"))
+			return;
+		
+		String fileName = f.getName();
+		String fileExt = Files.getFileExtension(fileName).toLowerCase();
+		
+		boolean isImage = imageExtensions.contains(fileExt);
+		boolean isAudio= audioExtensions.contains(fileExt);
+		boolean isVideo = videoExtensions.contains(fileExt);
+				
+		if (!isImage && !isAudio && !isVideo)
+			return;
+		
+		FileInputStream mediaStream = new FileInputStream(f);
+		Metadata metadata = null; 
+		try {
+			metadata = ImageMetadataReader.readMetadata(mediaStream);		
+		}
+		catch(ImageProcessingException ipe)
+		{
+			System.err.println("Error reading " + fileName + ": " + ipe);
+		}
+		catch(StringIndexOutOfBoundsException sobe)
+		{
+			System.err.println("Error reading " + fileName + ": " + sobe);
+		}		
+		
+		Directory directory = null; 
+		String dateTimeOrigStr = null;
+		if (metadata != null)
+		{
+			if (isImage)
+				directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+			else if (isVideo)
+			{
+				if (fileExt.equals("mp4"))
+					directory = metadata.getFirstDirectoryOfType(Mp4Directory.class);
+				else if (fileExt.equals("mov"))
+					directory = metadata.getFirstDirectoryOfType(QuickTimeMediaDirectory.class);				
+			}
+			else if (isAudio && fileExt.equals("wav"))
+					directory = metadata.getFirstDirectoryOfType(WavDirectory.class);
+			
+			if (directory != null)
+			{
+		        if (isImage)
+		        	dateTimeOrigStr = directory.getString(ExifDirectoryBase.TAG_DATETIME_ORIGINAL);
+		        else if (isVideo)
+		        	if (fileExt.equals("mp4"))
+		        		dateTimeOrigStr = directory.getString(Mp4Directory.TAG_CREATION_TIME);
+					else if (fileExt.equals("mov"))
+						dateTimeOrigStr = directory.getString(QuickTimeMediaDirectory.TAG_CREATION_TIME);
+		        else if (isAudio)
+		        	dateTimeOrigStr = directory.getString(WavDirectory.TAG_DATE_CREATED);	
+			}
+		}
+        
+		LocalDateTime mediaTime = null;
+		if (dateTimeOrigStr == null)
+		{	//use file last modified date if we can't get anything from EXIF
+			dateTimeOrigStr = new Date(f.lastModified()).toString();
+			if (dateTimeOrigStr == null)
+				return;
+			mediaTime = LocalDateTime.parse(dateTimeOrigStr,videoDtf);
+		}
+		else
+		{
+			try 
+			{
+				if (isImage)
+					mediaTime = LocalDateTime.parse(dateTimeOrigStr,imageDtf);
+				else
+					mediaTime = LocalDateTime.parse(dateTimeOrigStr,videoDtf);					
+			} 
+			catch (DateTimeParseException dtpe) 
+			{
+				System.err.println("Can't find creation time for " + fileName);
+				return;
+			}
+		}
+		
+		if (hrsOffset != 0)
+			mediaTime = mediaTime.plusHours(hrsOffset);		
+		
+		System.out.println(fileName + ": " + mediaTime);
+
+		String datePath = outputPath + File.separator + mediaTime.format(folderDtf);
+		
+		File dateDir = new File(datePath);
+		if (!dateDir.exists())
+			dateDir.mkdir();
+		
+		String subId = null;
+		if (mediaTime != null)					
+			subId =	rangeMap.get(mediaTime);	
+		File movedFile;
+		if (subId != null)
+		{
+			String subIdPath = datePath + File.separator + subId;
+			File subIdDir = new File(subIdPath);
+			if (!subIdDir.exists())
+				subIdDir.mkdir();
+			
+			movedFile = new File(subIdPath + File.separator + f.getName());
+			
+			subIds.add(subId);
+			statsMap.get(subId).incNumAssetsLocal();
+		}
+		else
+			movedFile = new File(datePath + File.separator + f.getName());
+		
+		System.out.println("Moving " + fileName + " to " + movedFile.getPath());
+		Files.move(f,movedFile);
+	}
+	
+	public static void main(String[] args) throws IOException
+	{
+		if (args.length < 2)
+		{
+			System.out.println("USAGE: 1st argument is path to directory that contains MyEBirdData.csv file (e.g. downloads/ebird). 2nd argument is path to media directory.");
+			return;
+		}
+		
+		parseCsv(args[0]);
 		
 		String imagePath = args[1];
 		File imagePathFile = new File(imagePath);
-		Iterable<File> traverser = Files.fileTraverser().depthFirstPreOrder(imagePathFile);
 		
-		String outputPath = imagePath + File.separator + "ebird";		
-		
+		String outputPath = imagePath + File.separator + "ebird";
 		File outputDir = new File(outputPath);
 		if (!outputDir.exists())
 			outputDir.mkdir();			
 		
-		Set<String> subIds = new TreeSet<>();		
+		Set<String> subIds = new TreeSet<>();
+		Long hrsOffset= args.length == 3 ? Long.valueOf(args[2]) : 0l;
+		Iterable<File> traverser = Files.fileTraverser().depthFirstPreOrder(imagePathFile);
 		for (File f:traverser)
-		{
-			if (f.isDirectory() || f.getPath().contains("ebird"))
-				continue;
-			
-			String fileName = f.getName();
-			String fileExt = Files.getFileExtension(fileName).toLowerCase();
-			
-			boolean isImage = imageExtensions.contains(fileExt);
-			boolean isAudio= audioExtensions.contains(fileExt);
-			boolean isVideo = videoExtensions.contains(fileExt);
-					
-			if (!isImage && !isAudio && !isVideo)
-				continue;
-			
-			FileInputStream mediaStream = new FileInputStream(f);
-			Metadata metadata = null; 
-			try {
-				metadata = ImageMetadataReader.readMetadata(mediaStream);		
-			}
-			catch(ImageProcessingException ipe)
-			{
-				System.err.println("Error reading " + fileName + ": " + ipe);
-			}
-			catch(StringIndexOutOfBoundsException sobe)
-			{
-				System.err.println("Error reading " + fileName + ": " + sobe);
-			}		
-			
-			Directory directory = null; 
-			String dateTimeOrigStr = null;
-			if (metadata != null)
-			{
-				if (isImage)
-					directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-				else if (isVideo)
-				{
-					if (fileExt.equals("mp4"))
-						directory = metadata.getFirstDirectoryOfType(Mp4Directory.class);
-					else if (fileExt.equals("mov"))
-						directory = metadata.getFirstDirectoryOfType(QuickTimeMediaDirectory.class);				
-				}
-				else if (isAudio && fileExt.equals("wav"))
-						directory = metadata.getFirstDirectoryOfType(WavDirectory.class);
-				
-				if (directory != null)
-				{
-			        if (isImage)
-			        	dateTimeOrigStr = directory.getString(ExifDirectoryBase.TAG_DATETIME_ORIGINAL);
-			        else if (isVideo)
-			        	if (fileExt.equals("mp4"))
-			        		dateTimeOrigStr = directory.getString(Mp4Directory.TAG_CREATION_TIME);
-						else if (fileExt.equals("mov"))
-							dateTimeOrigStr = directory.getString(QuickTimeMediaDirectory.TAG_CREATION_TIME);
-			        else if (isAudio)
-			        	dateTimeOrigStr = directory.getString(WavDirectory.TAG_DATE_CREATED);	
-				}
-			}
-	        
-			LocalDateTime mediaTime = null;
-			if (dateTimeOrigStr == null)
-			{	//use file last modified date if we can't get anything from EXIF
-				dateTimeOrigStr = new Date(f.lastModified()).toString();
-				if (dateTimeOrigStr == null)
-					continue;
-				mediaTime = LocalDateTime.parse(dateTimeOrigStr,videoDtf);
-			}
-			else
-			{
-				try 
-				{
-					if (isImage)
-						mediaTime = LocalDateTime.parse(dateTimeOrigStr,imageDtf);
-					else
-						mediaTime = LocalDateTime.parse(dateTimeOrigStr,videoDtf);					
-				} 
-				catch (DateTimeParseException dtpe) 
-				{
-					System.err.println("Can't find creation time for " + fileName);
-					continue;
-				}
-			}
-			
-			if (hrsOffset != 0)
-				mediaTime = mediaTime.plusHours(hrsOffset);		
-			
-			System.out.println(fileName + ": " + mediaTime);
-	
-			String datePath = outputPath + File.separator + mediaTime.format(folderDtf);
-			
-			File dateDir = new File(datePath);
-			if (!dateDir.exists())
-				dateDir.mkdir();
-			
-			String subId = null;
-			if (mediaTime != null)					
-				subId =	rangeMap.get(mediaTime);	
-			File movedFile;
-			if (subId != null)
-			{
-				String subIdPath = datePath + File.separator + subId;
-				File subIdDir = new File(subIdPath);
-				if (!subIdDir.exists())
-					subIdDir.mkdir();
-				
-				movedFile = new File(subIdPath + File.separator + f.getName());
-				
-				subIds.add(subId);
-				statsMap.get(subId).incNumAssetsLocal();
-			}
-			else
-				movedFile = new File(datePath + File.separator + f.getName());
-			
-			System.out.println("Moving " + fileName + " to " + movedFile.getPath());
-			Files.move(f,movedFile);
-		}
+			checkMetadataAndMove(f,outputPath,hrsOffset,subIds);
 		
 		if (!subIds.isEmpty())
 		{
