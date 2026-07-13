da1169f45ae16f2954c5baec2eb43890adee3ef8
Tim Lenz <tclenz@gmail.com>
2026-07-12 22:06:52 -0400

    Too much of a hassle to use separate branch for docs

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"fun.seabird.app"},{"l":"fun.seabird.provider"},{"l":"fun.seabird.sorter"},{"l":"fun.seabird.util"}];updateSearchResults();
